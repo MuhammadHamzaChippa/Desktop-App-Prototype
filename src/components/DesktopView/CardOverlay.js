6580250121e96dd22584c370bf08428462d48ea7
mhamzachippa <mhamzachippa@gmail.com>
2023-06-06 08:37:23 +0500

    Drag Overlay Cursor Center

--- src/components/DesktopView/CardOverlay.js
+++ src/components/DesktopView/CardOverlay.js
@@ -3,7 +3,7 @@ import { DragOverlay } from "@dnd-kit/core";
 import { snapCenterToCursor } from "@dnd-kit/modifiers";
 const CardOverlay = ({ card, index }) => {
 	return (
-		<DragOverlay>
+		<DragOverlay modifiers={[snapCenterToCursor]}>
 			{card && (
 				<div
 					className="bg-[white] border-solid border-[2px] border-[#29AAE1] w-[176px] h-[100px] rounded-[8px] flex items-center justify-center mt-[-88px]"
